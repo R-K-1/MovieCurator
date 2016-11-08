f86e744c49d6bb2879a21ab26ba695ff77c61ed2
R-K-1 <raymondkalonji@gmail.com>
2016-11-08 11:46:53 -0500

    Creating the Movie class.

---
+++ app/src/main/java/com/example/ray/popularmovies/Movie.java
@@ -0,0 +1,93 @@
+package com.example.ray.popularmovies;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Ray on 11/6/2016.
+ */
+
+public class Movie {
+    private BigDecimal id;
+    private String title;
+    private String originalTitle;
+    private String posterPath;
+    private String backdropPath;
+    private String overview;
+    private String releaseDate;
+    private BigDecimal popularity;
+
+    public Movie(BigDecimal id, String title, String posterPath, String backdropPath, String overview,
+                 String releaseDate, BigDecimal popularity) {
+        this.id = id;
+        this.title = title;
+        this.posterPath = posterPath;
+        this.backdropPath = backdropPath;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.popularity = popularity;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String originalTitle) {
+        this.title = title;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public BigDecimal getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(BigDecimal popularity) {
+        this.popularity = popularity;
+    }
+}
